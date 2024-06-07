c7d36f5b0819ef3c842d25b61aed0df217cdbb96
Josef Leite <josef@mbp-von-josef.home>
2024-06-07 13:16:58 +0200

    Das ist mein erstes eigner Framework

--- traversymvc/app/views/pages/index.php
+++ traversymvc/app/views/pages/index.php
@@ -1,3 +1,4 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
   <h1><?php echo $data['title']; ?></h1>
+  <p>This ist the TraversyMVC PHP Framework,pleas refer to the doc on how to use it </p>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
